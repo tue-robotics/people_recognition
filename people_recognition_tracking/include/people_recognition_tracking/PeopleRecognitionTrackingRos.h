1aa21fd0e032267bde8e87b103b484ce9986294c
Arpit Aggarwal <ar13pit@gmail.com>
2022-04-12 19:33:59 +0200

    wip

--- people_recognition_tracking/include/people_recognition_tracking/PeopleRecognitionTrackingRos.h
+++ people_recognition_tracking/include/people_recognition_tracking/PeopleRecognitionTrackingRos.h
@@ -1,7 +1,10 @@
 #ifndef __PEOPLE_RECOGNITION_TRACKING_ROS_H__
 #define __PEOPLE_RECOGNITION_TRACKING_ROS_H__
 
+#include <actionlib/server/simple_action_server.h>
+#include <people_recognition_msgs/TrackOperatorAction.h>
 #include <people_recognition_tracking/Tracking.h>
+#include <ros/ros.h>
 
 namespace people_recognition
 {
@@ -11,6 +14,9 @@ public:
   PeopleRecognitionTrackingRos();
   ~PeopleRecognitionTrackingRos();
 
+private:
+  ros::NodeHandle nh_, pnh_;
+  action
 };  // class PeopleRecognitionTrackingRos
 }  // namespace people_recognition
 
